424fef64fcfb3ea50449dc42589b12cd294e874a
Deleonwira <hamzadeleonw123@gmail.com>
2026-06-15 10:03:43 +0700

    setup springboot api

---
+++ backend-api/src/test/java/com/ogani/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ogani.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
